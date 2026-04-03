8b4039451f73ec7927844461256be1be65112be1
Ranjitha <ranjithaswamygowda2004@gmail.com>
2026-04-03 16:40:51 +0530

    updated backend url

--- frontend/lib/socket.ts
+++ frontend/lib/socket.ts
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-const SOCKET_URL = 'http://localhost:5000';
+const SOCKET_URL = 'https://skillzone-gblf.onrender.com';
 
 // We export a function to initialize the socket so we can control when it connects
 export const socket = io(SOCKET_URL, {
